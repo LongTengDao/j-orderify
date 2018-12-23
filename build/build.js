bfc7f049ef178677ffad7e106ad44dc7ffa61245
龙腾道 <LongTengDao@LongTengDao.com>
2018-12-23 21:06:51 +0800

    description

--- build/build.js
+++ build/build.js
@@ -6,14 +6,14 @@ require('../test/test.js')(async ({ build, read, copy }) => {
 		name: 'j-orderify',
 		Name: '@ltd/j-orderify',
 		Desc: `
-			返回一个可以保证给定对象的自有属性按照此后添加的顺序排列的 proxy，即便键名是 symbol，或整数式 string。
-			Return a proxy for the given object, which can guarantee the own keys are in setting order, even if the key name is a symbol or an integer-form string.`,
+			返回一个能保证给定对象的属性按此后添加顺序排列的 proxy，即使键名是 symbol，或整数 string。
+			Return a proxy for given object, which can guarantee own keys are in setting order, even if the key name is symbol or int string.`,
 		semver: await read('src/version'),
 		ES: 6,
 		ESM: true,
 		NPM: {
 			meta_: {
-				description: 'Return a proxy for the given object, which can guarantee the own keys are in setting order, even if the key name is a symbol or an integer-form string.／返回一个可以保证给定对象的自有属性按照此后添加的顺序排列的 proxy，即便键名是 symbol，或整数式 string。',
+				description: 'Return a proxy for given object, which can guarantee own keys are in setting order, even if the key name is symbol or int string.／返回一个能保证给定对象的属性按此后添加顺序排列的 proxy，即使键名是 symbol，或整数 string。',
 			}
 		},
 	});
