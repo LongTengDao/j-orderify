f22a5dfae04b0fae54bf62f6f2deae35954a1232
龙腾道 <LongTengDao@LongTengDao.com>
2019-02-24 10:29:15 +0800

    @ltd/j-dev

--- build/build.js
+++ build/build.js
@@ -1,6 +1,6 @@
 'use strict';
 
-require('../test/test.js')(async ({ build, read, copy }) => {
+require('../test/test.js')(async ({ build, get, map }) => {
 	
 	await build({
 		name: 'j-orderify',
@@ -8,7 +8,7 @@ require('../test/test.js')(async ({ build, read, copy }) => {
 		Desc: `
 			返回一个能保证给定对象的属性按此后添加顺序排列的 proxy，即使键名是 symbol，或整数 string。
 			Return a proxy for given object, which can guarantee own keys are in setting order, even if the key name is symbol or int string.`,
-		semver: await read('src/version'),
+		semver: await get('src/version'),
 		ES: 6,
 		ESM: true,
 		NPM: {
@@ -21,7 +21,7 @@ require('../test/test.js')(async ({ build, read, copy }) => {
 		},
 	});
 	
-	await copy(
+	await map(
 		'docs/README.md',
 		string => string.replace(/(\n```+)[^`\r\n]+/g, '$1'),
 		'dist/NPM/README.md',
