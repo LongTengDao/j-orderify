a59ce52d901b1045c34ff9007cd2cc305ac8535b
龙腾道 <LongTengDao@LongTengDao.com>
2019-03-05 14:50:36 +0800

    Update test.js

--- test/test.js
+++ test/test.js
@@ -6,22 +6,28 @@ module.exports = require('@ltd/j-dev')(__dirname+'/..')(async ({ import_ }) => {
 	
 	const object = {};
 	prepare(object);
-	compare(object, 'integer-string, string, symbol');
+	compare(object, 'integer-string, integer-string, string, string, symbol, symbol');
 	
 	let proxy = orderify({});
 	prepare(proxy);
-	compare(proxy, 'symbol, string, integer-string');
+	compare(proxy, 'symbol, string, integer-string, symbol, string, integer-string');
 	
 	proxy = new Orderified;
 	prepare(proxy);
-	compare(proxy, 'symbol, string, integer-string');
+	compare(proxy, 'symbol, string, integer-string, symbol, string, integer-string');
 	
 });
 
 function prepare (object) {
-	object[Symbol()] = 'symbol';
-	object.string = 'string';
+	
+	object[Symbol(1)] = 'symbol';
+	object.string1 = 'string';
 	object[1] = 'integer-string';
+	
+	object[Symbol(2)] = 'symbol';
+	object.string2 = 'string';
+	object[2] = 'integer-string';
+	
 }
 
 function compare (object, ownKeys) {
